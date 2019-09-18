d3aa634fe0f2cae51aa8fa6a7f05cb21dae493e8
inaba <software@panduro.guru>
2019-09-18 22:28:22 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <SDL2/SDL.h>
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* win = SDL_CreateWindow("Game", 
+                                     SDL_WINDOWPOS_CENTERED, 
+                                     SDL_WINDOWPOS_CENTERED, 
+                                     800, 
+                                     600, 
+                                     0);
+
+  while (1)
+    ;
+
+  return 0;
+}
